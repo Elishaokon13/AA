3a97b7a7f9234de3a3b49eb5d8993669fbd95603
Elishaokon13 <eliscodavid@gmail.com>
2023-11-12 11:31:06 +0100

    chore: Added variables

--- const.ts
+++ const.ts
@@ -1,3 +1,3 @@
-export const activeChain = "mumbai";
-export const factoryAddress = "0xA4a0b37823a19541D0d2e049cC935E6398b5AB9F";
-export const editionDropAddress = "0x3F01B89cef5257CCD0f37F631a40365Fb0F1886a";
+export const activeChain = "TelosEvmTestnet";
+export const factoryAddress = "0x23517eE47531e66c8c87b09D92612E7fc277F56F";
+export const editionDropAddress = "0x68113B867953D3c1b27a992888312f3286EFA09A";
