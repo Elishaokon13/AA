77189222ea3e8999ed9888784a8b705ced85d07d
Elishaokon13 <eliscodavid@gmail.com>
2023-11-12 12:57:20 +0100

    chore: corrected factoryAddress

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -5,13 +5,14 @@ import {
   embeddedWallet,
 } from "@thirdweb-dev/react";
 import "../styles/globals.css";
-import { activeChain, factoryAddress } from "../const";
+import { TelosEvmTestnet } from "@thirdweb-dev/chains"
+import { factoryAddress } from "../const";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <ThirdwebProvider
       clientId={process.env.NEXT_PUBLIC_TEMPLATE_CLIENT_ID}
-      activeChain={activeChain}
+      activeChain={ TelosEvmTestnet }
       supportedWallets={[
         smartWallet(embeddedWallet(), {
           factoryAddress: factoryAddress,
